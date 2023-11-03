d6f190f7bac57b6b009667a72f88b4f39692ce8b
cudo <Cuong.Do@optimizely.com>
2023-11-03 13:42:35 +0700

    build

--- config-overrides.js
+++ config-overrides.js
@@ -4,5 +4,6 @@ module.exports = function override(config, env) {
         path: require.resolve('path-browserify'),
         stream: require.resolve('stream-browserify'),
     };
+    config.optimization.splitChunks = { chunks: 'all', name: false };
     return config;
 }
